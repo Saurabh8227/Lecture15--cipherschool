b8c7b8768a21150faadcebd5ac77dea832b3ff75
Saurabh Pandey <119298223+Saurabh8227@users.noreply.github.com>
2024-07-14 00:23:58 +0530

    Add files via upload

---
+++ function3.cpp
@@ -0,0 +1,13 @@
+// To compare two numbers and the greatest integres 
+#include<iostream>
+using namespace std;
+int compare(int a, int b)
+{
+    if(a>b)
+    {
+        return a;
+    }
+    else{
+        return b;
+    }
+}
